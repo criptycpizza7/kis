587d3c2b62d8bfb3027cf1d528bb8faaccb38cfe
juna <junaart@mail.ru>
2021-05-15 10:26:52 +0300

    Автомат

---
+++ TheoryLanguage/Code/My.py
@@ -0,0 +1,67 @@
+class auto:
+    __q=[]
+    __abc=[]
+    __q0=None
+    __f=[]
+    __current = None
+    __delta=[]
+
+    def add_q(self,x):
+        if not(x in self.__q):
+            self.__q.append(x)
+
+    def add_abc(self,x):
+        if not(x in self.__abc):
+            self.__abc.append(x)
+
+    def add_f(self,x):
+        if x in self.__q:
+            self.__f.append(x)
+
+    def add_delta(self,x):
+        if isinstance(x,list):
+            if (len(x)==3):
+                if (x[0] in self.__q) & (x[1] in self.__abc) & (x[2] in self.__q):
+                    self.__delta.append(x)
+
+    def get_abc(self):
+        return self.__abc
+    def get_q(self):
+        return self.__q
+    def get_f(self):
+        return self.__f
+    def get_q0(self):
+        return self.__q0
+    def get_delta(self):
+        return self.__delta
+
+    def __init__(self, Q,ABC, D, F):
+        for i in Q:
+            self.add_q(i)
+        for i in ABC:
+            self.add_abc(i)
+        for i in F:
+            self.add_f(i)
+        self.__q0=Q[0]
+        for i in D:
+            self.add_delta(i)
+
+    def work(self,L):
+        self.__current=self.__q0
+        for i in L:
+            pr=False
+            for j in self.__delta:
+                if (j[0]==self.__current) & (i==j[1]):
+                    self.__current=j[2]
+                    pr=True
+                    break
+            if not(pr):
+                print("Аварийное завершение")
+                break
+        if (self.__current in self.__f):
+            return True
+        else:
+            return False
+
+    
+A=auto(["A", "B", "C", "D", "F"],["1","2","3"], [["A","1","B"], ["A","2","C"], ["A","3","F"], ["B","1","C"],["B","2","F"], ["B","3","D"], ["C","1","F"], ["C","2","D"], ["C","3","D"],["D","1","B"], ["D","2","C"],["D","3","F"],["F","1","B"], ["F","2","C"], ["F","3","F"]],["F"])
